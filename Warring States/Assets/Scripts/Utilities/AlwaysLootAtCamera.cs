918bffb2c4722b9db2caa3b62c7257bc64ceceeb
uqtony <uqtony@gmail.com>
2022-11-09 04:00:30 +0800

    Add Always Look at camera
    The game object would always face to camera.

---
+++ Warring States/Assets/Scripts/Utilities/AlwaysLootAtCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysLootAtCamera : MonoBehaviour
+{
+    [SerializeField]
+    Camera _camera;
+
+    private void Update()
+    {
+
+        if (_camera == null)
+            _camera = Camera.main;
+        transform.forward = _camera.transform.forward;
+
+    }
+}
